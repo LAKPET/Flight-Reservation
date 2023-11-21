f256ec9cf6ccce6bafc41de2fa8439fb20b5613d
kunza10153 <yuvakron007@gmail.com>
2023-11-21 16:59:03 +0700

    pa

--- NokKapood_Travel/NokKapood_Travel/settings.py
+++ NokKapood_Travel/NokKapood_Travel/settings.py
@@ -91,12 +91,23 @@ WSGI_APPLICATION = 'NokKapood_Travel.wsgi.application'
 # }
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'project',
+#         'USER': 'postgres',
+#         'PASSWORD': '0616399791Za',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     } 
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'project',
         'USER': 'postgres',
-        'PASSWORD': '0616399791Za',
+        'PASSWORD': 'kunza!!!10153',
         'HOST': 'localhost',
         'PORT': '5432',
     } 
